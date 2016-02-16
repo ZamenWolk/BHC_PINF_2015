9c7d90804d7c52e0b54de6efea1df06e9eb543c4
Evolus3r <samonil@hotmail.fr>
2016-02-16 18:50:56 +0100

    Modification de différentes pages
    Modifier pour qu'elle aille bien avec le header et footer.

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <?php
+//include_once ("subscribe.html");
 include_once("header.html");
-include_once ("subscribe.html");
+if(isset($_GET["url"]))
+    $choix = $_GET["url"];
+else
+    $choix = "";
+switch($choix)
+{
+    case "recherche":
+
+        include_once("recherche.html");
+
+        break;
+    case "admin":
+
+        include_once ("Admin.html");
+
+        break;
+    case "inscription":
+        include_once ("subscribe.html");
+        break;
+    default: //TODO : A définir
+        //include_once("");
+        break;
+}
 include_once("footer.html");
+
 ?>
