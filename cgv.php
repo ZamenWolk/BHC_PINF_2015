5849c15e5531f62e1f9063ec509a4d360cf07c41
Andoslash <andoslash@hotmail.fr>
2016-03-03 21:59:58 +0100

    CGV (suite et fin)
    Fin de la mise en place de la CGV, il manque des informations qui sont à
    demandé au client

--- cgv.php
+++ cgv.php
@@ -6,7 +6,7 @@ if(0)//TODO: implémentation de la connection au site Si mauvaise connection etc
 include_once("templates/header.php");
 ?>
 
-<h2 style="text-align:center"> Conditions générales de vente </h2>
+<h3 style="text-align:center"> Conditions générales de vente </h2>
 <div>
 	<p> <strong>JSPneus</strong> </br>
 		Société (Donnez le statut) au capital de X euros </br>
@@ -18,28 +18,65 @@ include_once("templates/header.php");
 		Conditions générales de vente des produits vendus sur  <a href="http://www.js-pneus.fr">js-pneus.fr</a> </br>
 		Date de dernière mise à jour 02 Mars 2016 </br>
 	</p>
-	<h3> Article 1 - Objet </h3>
-	<p>	Les présentes conditions régissent les ventes par la société JSPneus, MAGNICOURT EN COMTE (62127) de pneus.
+	<h4>Article 1 - Objet </h4>
+		<p>	Les présentes conditions régissent les ventes par la société JSPneus, MAGNICOURT EN COMTE (62127) de pneus.
+		</p>
+	<h4>Article 2 - Prix </h4>
+		<p>Les prix de nos produits sont indiqués en euros toutes taxes comprises (TVA et autres taxes applicables au jour de la commande), sauf indication contraire et hors frais de traitement et d'expédition. </br>
+		En cas de commande vers un pays autre que la France métropolitaine vous êtes l'importateur du ou des produits concernés. Des droits de douane ou autres taxes locales ou droits d'importation ou taxes d'état sont susceptibles d'être exigibles. Ces droits et sommes ne relèvent pas du ressort de la société <strong>JSPneus</strong>. Ils seront à votre charge et relèvent de votre entière responsabilité, tant en termes de déclarations que de paiements aux autorités et organismes compétents de votre pays. Nous vous conseillons de vous renseigner sur ces aspects auprès de vos autorités locales. Toutes les commandes quelle que soit leur origine sont payables en euros.  La société <strong>JSPneus</strong> se réserve le droit de modifier ses prix à tout moment, mais le produit sera facturé sur la base du tarif en vigueur au moment de la validation de la commande et sous réserve de disponibilité. Les produits demeurent la propriété de la société <strong>JSPneus</strong> jusqu'au paiement complet du prix. Attention : dès que vous prenez possession physiquement des produits commandés, les risques de perte ou d'endommagement des produits vous sont transférés. 
 	</p>
-	<h3> Article 2 - Prix </h3>
-	<p>Les prix de nos produits sont indiqués en euros toutes taxes comprises (TVA et autres taxes applicables au jour de la commande), sauf indication contraire et hors frais de traitement et d'expédition. </br>
-	En cas de commande vers un pays autre que la France métropolitaine vous êtes l'importateur du ou des produits concernés. Des droits de douane ou autres taxes locales ou droits d'importation ou taxes d'état sont susceptibles d'être exigibles. Ces droits et sommes ne relèvent pas du ressort de la société <strong>JSPneus</strong>. Ils seront à votre charge et relèvent de votre entière responsabilité, tant en termes de déclarations que de paiements aux autorités et organismes compétents de votre pays. Nous vous conseillons de vous renseigner sur ces aspects auprès de vos autorités locales. Toutes les commandes quelle que soit leur origine sont payables en euros.  La société <strong>JSPneus</strong> se réserve le droit de modifier ses prix à tout moment, mais le produit sera facturé sur la base du tarif en vigueur au moment de la validation de la commande et sous réserve de disponibilité. Les produits demeurent la propriété de la société <strong>JSPneus</strong> jusqu'au paiement complet du prix. Attention : dès que vous prenez possession physiquement des produits commandés, les risques de perte ou d'endommagement des produits vous sont transférés. 
+	<h4>Article 3 - Commandes </h4>
+		<p> Vous pouvez passez commande directement sur le site <a href="http://www.js-pneus.fr">js-pneus.fr</a>
+		Les informations contractuelles sont présentées en langue française et feront l'objet d'une confirmation au plus tard au moment de la validation de votre commande. La société <strong>JSPneus</strong> se réserve le droit de ne pas enregistrer une commande et de ne pas la confirmer pour quelque raison que ce soit, et plus particulièrement en cas de problème d'approvisionnement, ou en cas de difficulté concernant la commande reçue.   
 	</p>
-	<h3> Article 3 - Commandes </h3>
-	<p> Vous pouvez passez commande directement sur le site <a href="http://www.js-pneus.fr">js-pneus.fr</a>
-	Les informations contractuelles sont présentées en langue française et feront l'objet d'une confirmation au plus tard au moment de la validation de votre commande. La société <strong>JSPneus</strong> se réserve le droit de ne pas enregistrer une commande et de ne pas la confirmer pour quelque raison que ce soit, et plus particulièrement en cas de problème d'approvisionnement, ou en cas de difficulté concernant la commande reçue.   
+	<h4>Article 4 - Validation de votre commande </h4>
+		<p>Toute commande figurant sur le site Internet <a href="http://www.js-pneus.fr">js-pneus.fr</a> suppose l'adhésion aux présentes Conditions Générales. Toute confirmation de commande entraîne votre adhésion pleine et entière aux présentes conditions générales de vente, sans exception ni réserve. L'ensemble des données fournies et la confirmation enregistrée vaudront preuve de la transaction. Vous déclarez en avoir parfaite connaissance. La confirmation de commande vaudra signature et acceptation des opérations effectuées. Un récapitulatif des informations de votre commande et des présentes Conditions Générales, vous sera communiqué en format PDF (Portable Document Format) via l'adresse e-mail de confirmation de votre commande. 
 	</p>
-	<h3> Article 4 - Validation de votre commande </h3>
-	<p>Toute commande figurant sur le site Internet <a href="http://www.js-pneus.fr">js-pneus.fr</a> suppose l'adhésion aux présentes Conditions Générales. Toute confirmation de commande entraîne votre adhésion pleine et entière aux présentes conditions générales de vente, sans exception ni réserve. L'ensemble des données fournies et la confirmation enregistrée vaudront preuve de la transaction. Vous déclarez en avoir parfaite connaissance. La confirmation de commande vaudra signature et acceptation des opérations effectuées. Un récapitulatif des informations de votre commande et des présentes Conditions Générales, vous sera communiqué en format PDF (Portable Document Format) via l'adresse e-mail de confirmation de votre commande. 
+	<h4>Article 5 - Paiement </h4>
+		<p> Le fait de valider votre commande implique pour vous l'obligation de payer le prix indiqué. Le réglement de vos achats peut s'effectuer en chèque bancaire ou en espèce lors de la réception de vos produits, directement chez vous ou chez <strong>JSPneus</strong>
 	</p>
-	<h3> Article 5 - Paiement </h3>
-	<p> Le fait de valider votre commande implique pour vous l'obligation de payer le prix indiqué. Le réglement de vos achats peut s'effectuer en chèque bancaire ou en espèce lors de la réception de vos produits, directement chez vous ou chez <strong>JSPneus</strong>
-	</p>
-	<h3> Article 6 - Rétractation </h3>
-	<p> Conformément aux dispositions de l'article L.121-21 du Code de la Consommation, vous disposez d'un délai de rétractation de 14 jours à compter de la réception de vos produits pour exercer votre droit de rétraction sans avoir à justifier de motifs ni à payer de pénalité. Les retours sont à effectuer dans leur état d'origine et complets (emballage, accessoires, notice). Dans ce cadre, votre responsabilité est engagée. Tout dommage subi par le produit à cette occasion peut être de nature à faire échec au droit de rétractation. Les frais de retour sont à votre charge. En cas d'exercice du droit de rétractation, la société <strong>JSPneus</strong> procédera au remboursement des sommes versées, dans un délai de 14 jours suivant la notification de votre demande et via le même moyen de paiement que celui utilisé lors de la commande. 
+	<h4>Article 6 - Rétractation </h4>
+		<p> Conformément aux dispositions de l'article L.121-21 du Code de la Consommation, vous disposez d'un délai de rétractation de 14 jours à compter de la réception de vos produits pour exercer votre droit de rétraction sans avoir à justifier de motifs ni à payer de pénalité. Les retours sont à effectuer dans leur état d'origine et complets (emballage, accessoires, notice). Dans ce cadre, votre responsabilité est engagée. Tout dommage subi par le produit à cette occasion peut être de nature à faire échec au droit de rétractation. Les frais de retour sont à votre charge. En cas d'exercice du droit de rétractation, la société <strong>JSPneus</strong> procédera au remboursement des sommes versées, dans un délai de 14 jours suivant la notification de votre demande et via le même moyen de paiement que celui utilisé lors de la commande. 
 	</p>
 	<strong>EXCEPTIONS AU DROIT DE RETRACTATION</strong></br>
-	<p>Conformément aux dispositions de l'article L.121-21-8 du Code de la Consommation, le droit de rétractation ne s'applique pas à : </p> 
+		<p>Conformément aux dispositions de l'article L.121-21-8 du Code de la Consommation, le droit de rétractation ne s'applique pas à : </p> 
+	<ul>
+		<li>La fourniture de services pleinement exécutés avant la fin du délai de rétractation et dont l'exécution a commencé après accord préalable exprès du consommateur et renoncement exprès à son droit de rétractation.</li>
+		<li>La fourniture de biens ou de services dont le prix dépend de fluctuations sur le marché financier échappant au contrôle du professionnel et susceptibles de se produire pendant le délai de rétractation. </li>
+		<li>La fourniture de biens confectionnés selon les spécifications du consommateur ou nettement personnalisés.</li>
+		<li>La fourniture de biens susceptibles de se détériorer ou de se périmer rapidement. </li>
+		<li>La fourniture de biens qui ont été descellés par le consommateur après la livraison et qui ne peuvent être renvoyés pour des raisons d'hygiène ou de protection de la santé. </li>
+		<li>La fourniture de biens qui, après avoir été livrés et de par leur nature, sont mélangés de manière indissociable avec d'autres articles ;</li>
+		<li>La fourniture de boissons alcoolisées dont la livraison est différée au-delà de trente jours et dont la valeur convenue à la conclusion du contrat dépend de fluctuations sur le marché échappant au contrôle du professionnel. </li>
+		<li>La fourniture d'enregistrements audio ou vidéo ou de logiciels informatiques lorsqu'ils ont été descellés par le consommateur après la livraison. </li>
+		<li>La fourniture d'un journal, d'un périodique ou d'un magazine, sauf pour les contrats d'abonnement à ces publications. </li>
+		<li>Les transactions conclues lors d'une enchère publique.</li>
+		<li>La fourniture d'un contenu numérique non fourni sur un support matériel dont l'exécution a commencé après accord préalable exprès du consommateur et renoncement exprès à son droit de rétractation.</li>
+	</ul>
+	<h4>Article 7 - Disponibilité </h4>
+		<p>Nos produits sont proposés tant qu'ils sont visibles sur le site <a href="http://www.js-pneus.fr">js-pneus.fr</a> et dans la limite des stocks disponibles. Pour les produits non stockés, nos offres sont valables sous réserve de disponibilité chez nos fournisseurs. En cas d'indisponibilité de produit après passation de votre commande, nous vous en informerons par mail. Votre commande sera automatiquement annulée. En outre, le site Internet <a href="http://www.js-pneus.fr">js-pneus.fr</a> n'a pas vocation à vendre ses produits en quantités importantes. Par conséquent la société <strong>JSPneus</strong> se réserve le droit de refuser les commandes de pneus en quantité supérieure à X
+		</p>
+	<h4>Article 8 - Livraison </h4>
+		<p>Les produits sont livrés à l'adresse de livraison indiquée au cours du processus de commande, dans un délai établi par <strong>JSPneus</strong>. En cas de retard d'expédition, un mail vous sera adressé pour vous informer d'une éventuelle conséquence sur le délai de livraison qui vous a été indiqué. Conformément aux dispositions légales, en cas de retard de livraison, vous bénéficiez de la possibilité d'annuler la commande dans les conditions et modalités définies à l'article L 138-2 du Code de la Consommation. Si entre temps vous recevez le produit nous procéderons à son remboursement et aux frais d'acheminement dans les conditions de l'article L 138-3 du Code de la Consommation. En cas de livraisons par une transporteur, la société <strong>JSPneus</strong> ne peut être tenue pour responsable de retard de livraison dû exclusivement à une indisponibilité du client après plusieurs propositions de rendez-vous. 
+		</p>
+	<h4>Article 9- Garantie </h4>
+		<p>Tous nos produits bénéficient de la garantie légale de conformité et de la garantie des vices cachés, prévues par les articles 1641 et suivants du Code civil. En cas de non-conformité d'un produit vendu, il pourra être retourné, échangé ou remboursé. Toutes les réclamations, demandes d'échange ou de remboursement doivent s'effectuer par mail ou téléphone dans le délai de 30 jours de la livraison. Les produits doivent nous être retournés dans l'état dans lequel vous les avez reçus avec l'ensemble des éléments (accessoires, emballage, notice...). Les frais d'envoi vous seront remboursés sur la base du tarif facturé et les frais de retour vous seront remboursés sur présentation des justificatifs. Les dispositions de cet article ne vous empêchent pas de bénéficier du droit de rétractation prévu à l'article 6. 
+		</p>
+	<h4>Article 10 - Responsabilités </h4>
+		<p>Les produits proposés sont conformes à la législation française en vigueur. La responsabilité de la société <strong>JSPneus</strong> ne saurait être engagée en cas de non-respect de la législation du pays où le produit est livré. Il vous appartient de vérifier auprès des autorités locales les possibilités d'importation ou d'utilisation des produits ou services que vous envisagez de commander. Par ailleurs, la société <strong>JSPneus</strong> ne saurait être tenue pour responsable des dommages résultant d'une mauvaise utilisation du produit acheté. Enfin la responsabilité de la société <strong>JSPneus</strong> ne saurait être engagée pour tous les inconvénients ou dommages inhérents à l'utilisation du réseau Internet, notamment une rupture de service, une intrusion extérieure ou la présence de virus informatiques. 
+		</p>
+	<h4>Article 11 - Droits applicables en cas de litiges </h4>
+		<p>La langue du présent contrat est la langue française. Les présentes conditions de vente sont soumises à la loi française. En cas de litige, les tribunaux français seront les seuls compétents. 
+		</p>
+	<h4>Article 12 - Propriétés intellectuelles </h4>
+		<p>Tous les éléments du site <a href="http://www.js-pneus.fr">js-pneus.fr</a> sont et restent la propriété intellectuelle et exclusive de la société <strong>JSPneus</strong>. Nul n'est autorisé à reproduire, exploiter, rediffuser, ou utiliser à quelque titre que ce soit, même partiellement, des éléments du site qu'ils soient logiciels, visuels ou sonores. Tout lien simple ou par hypertexte est strictement interdit sans un accord écrit exprès de la société <strong>JSPneus</strong>.  
+		</p>
+	<h4>Article 13 - Données personnelles </h4>
+		<p>La société <strong>JSPneus</strong> se réserve le droit de collecter les informations nominatives et les données personnelles vous concernant. Elles sont nécessaires à la gestion de votre commande, ainsi qu'à l'amélioration des services et des informations que nous vous adressons. Elles peuvent aussi être transmises aux sociétés qui contribuent à ces relations, telles que celles chargées de l'exécution des services et commandes pour leur gestion, exécution, traitement et paiement. Ces informations et données sont également conservées à des fins de sécurité, afin de respecter les obligations légales et réglementaires. Conformément à la loi du 6 janvier 1978, vous disposez d'un droit d'accès, de rectification et d'opposition aux informations nominatives et aux données personnelles vous concernant, directement sur le site Internet. 
+		</p>
+	<h4> Article 14 - Archivage preuve </h4>
+		<p>La société <strong>JSPneus</strong> archivera les bons de commandes et les factures sur un support fiable et durable constituant une copie fidèle conformément aux dispositions de l'article 1348 du Code civil. Les registres informatisés de la société <strong>JSPneus</strong> seront considérés par toutes les parties concernées comme preuve des communications et commandes intervenus entre les parties. 
+		</p>
 </div>
 
 <?php
