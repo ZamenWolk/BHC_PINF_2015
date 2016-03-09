c105dd0e67f2c68f795834753265fcc9324646b3
Andoslash <andoslash@hotmail.fr>
2016-03-09 17:22:49 +0100

    Ajout page contact
    Ajout de la page contact.php
    L'aspect html est réalisé.
    Le début de la partie Ajax/jquery aussi mais reste à complétez

--- www/contact.php
+++ www/contact.php
@@ -1,27 +1,58 @@
 <?php
 include_once('header.php');
+
 ?>
+
+
 <div class=" well well-lg">
 <h1> Contact </h1>
 <form action="" method="post">
+<div>
+        <label id = "ContactObj" for="nom">Objet :</label></br>
+        <input type="text" id="obj" style="width:40%;" />
+    </div>
     <div>
-        <label for="nom">Nom :</label></br>
+        <label id = "ContactNom" for="nom">Nom :</label></br>
         <input type="text" id="nom" style="width:40%;" />
     </div>
     <div>
-        <label for="courriel">Adresse mail :</label></br>
+        <label id = "ContactMail" for="courriel">Adresse mail :</label></br>
         <input type="email" id="mail" style="width:40%;" />
     </div>
     <div>
-        <label for="message">Message :</label></br>
+        <label id = "ContactMessage" for="message">Message :</label></br>
         <textarea style = "resize:none; width:100%; height:200px" id="message"></textarea>
     </div>
     
-    <div class="button">
+    <div id="BoutonEnvoi" class="button">
         <button type="submit">Envoyer votre message</button>
     </div>
 </form>
 </div>
+<script>
+$('#BoutonEnvoi').click(function(){
+	$.ajax({
+	type: “POST”,
+	url: “send_mail.php”,
+	data: {
+    ‘message’: {
+      ‘from_email’: martin.canivez@gmail.com’,
+      ‘to’: [
+          {
+            ‘email’: ‘$('#ContactMail').val();’,
+            ‘name’: ‘$('#ContactNom').val()’,
+            ‘type’: ‘to’
+          },
+        ],
+      ‘autotext’: ‘true’,
+      ‘subject’: ‘$('#ContactObj').val();’,
+      ‘html’: ‘$('#ContactMessage').val();’
+    }
+  }
+ }).done(function(response) {
+   console.log(response);
+ });
+</script>
 <?php
 include_once('footer.php');
 ?>
