27c5c41807d8f121cc2b4ac10a7500f129d3d53f
Martin Canivez <martin.canivez@ig2i.fr>
2016-03-10 15:35:18 +0100

    Ajout du template doc d'ajax_base.php

--- assets/php/ajax/ajax_base.php
+++ assets/php/ajax/ajax_base.php
@@ -1,5 +1,26 @@
 <?php
 
+/**
+ * Fichier utilisant la méthode "#methode utilisée#"
+ * Actions possible :
+ *
+ * "#action1#"
+ * #description de l'action#
+ * Arguments : #Remplacer bloc par "Aucun argument si pas d'arguments#
+ * [    "#agument1#" => #Description de l'argument#,
+ *      "#agument2#" => #Description de l'argument# ]
+ * Renvoi :
+ * [    "#renvoi1#" => #Description du renvoi#,
+ *      "#renvoi2#" => #Description du renvoi# ]
+ * Avertit si : #Supprimer ce bloc si jamais d'avertissement#
+ *      - #Cause d'avertissement 1# (code #code d'avertissement#)
+ *      - #Cause d'avertissement 2# (code #code d'avertissement#)
+ * Echoue si : #Supprimer ce bloc si jamais d'erreur#
+ *      - #Cause d'erreur 1# (code #code d'erreur#)
+ *      - #Cause d'erreur 2# (code #code d'erreur#)
+ *
+ */
+
 if (!isset($_POST["action"]))
 {
     ajaxError("Action non définie");
