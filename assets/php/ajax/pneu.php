d60d78bb7c6df896d8ef235c879fd9409ceac093
Martin Canivez <martin.canivez@ig2i.fr>
2016-03-10 10:09:20 +0100

    Documentation du fichier AJAX pneu

--- assets/php/ajax/pneu.php
+++ assets/php/ajax/pneu.php
@@ -2,6 +2,24 @@
 include_once("../fonctions/AJAX.php");
 include_once("../fonctions/Pneu.php");
 
+/**
+ * Fichier utilisant la méthode "POST"
+ * Actions possible :
+ *
+ * "stockPneu"
+ * Permet de récupérer le stock d'un pneu en BDD
+ * Arguments :
+ * [    "referencePneu" => Référence du pneu,
+ *      "dateAjoutBDD"  => Date d'ajout du pneu en BDD *optionnel, cherche un pneu valable par défaut*]
+ * Renvoi :
+ * [    "referencePneu" => Référence du pneu,
+ *      "dateAjoutBDD"  => Date d'ajout du pneu en BDD *absent si l'argument "dateAjoutBDD" n'a pas été précisé*,
+ *      "stock"         => Stock du pneu en BDD ]
+ * Echoue si :
+ *      - La référence du pneu est vide
+ *      - La référence n'existe pas en BDD
+ */
+
 if (!isset($_POST["action"]))
 {
     ajaxError("Action non définie");
