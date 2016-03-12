17736f03ec176a36382e7d06685438e3fd2f8a44
Andoslash <andoslash@hotmail.fr>
2016-03-12 21:41:19 +0100

    Finalisation de la FAQ en fonction du client
    Le client m'a donné des infos pour la FAQ

--- www/faq.php
+++ www/faq.php
@@ -11,7 +11,7 @@ include_once("header.php");
     </div>
 	<div class="row"> 
 		<div class="col-lg-12">
-			<h2>Articles et commandes</h2>
+			<h2 style="color:white">Articles et commandes</h2>
 		</div>
 	</div>
     <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
@@ -32,7 +32,7 @@ include_once("header.php");
                     <p>A tout moment vous pouvez accéder à l'option de recherche de produit depuis la barre de navigation située en haut du site sous le nom "Recherche".
 					</br>En cliquant sur cette option, une barre de filtre de recherche s'ouvrira et vous permettra de choisir différents critères pour vos pneus.
 					</br>Vous pouvez également passer par le biais du catalogue des pneus pour avoir une vision plus globale pour votre recherche.
-					</br>Pour ce faire cliquer sur "Catalogue de pneus" dans la barre de navigation, vous aurez ainsi le choix parmis les différentes catégories. En cliquant sur l'une d'entre elles, vous serez redirigé vers le catalogue de la catégorie en question.
+					</br>Pour ce faire cliquez sur "Catalogue de pneus" dans la barre de navigation, vous aurez ainsi le choix parmis les différentes catégories. En cliquant sur l'une d'entre elles, vous serez redirigé vers le catalogue de la catégorie en question.
                     </p>
                 </div>
             </div>
@@ -73,7 +73,7 @@ include_once("header.php");
             </div>
             <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
                 <div class="panel-body">
-                    <p> A REMPLIR
+                    <p> Dans la barre de navigation, si vous êtes connecté, dans l'option "Mon Compte" vous pourrez accéder à l'option "Mes Commandes" reprenant votre commande actuelle et vos commandes passées auparavant.
                     </p>
                 </div>
             </div>
@@ -92,85 +92,86 @@ include_once("header.php");
             </div>
             <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
                 <div class="panel-body">
-                    <p>Les commandes sont envoyés à l'administrateur du site qui se charge de la partie de validation. Si vous souhaitez modifier ou annuler une commande, veuillez entrer en contact avec l'administrateur dans les plus brefs délais via la section "Contact"
+                    <p>Les commandes sont envoyées à l'administrateur du site qui se charge de la partie de validation. Si vous souhaitez modifier ou annuler une commande, veuillez entrer en contact avec l'administrateur dans les plus brefs délais via la section "Contact"
                     </p>
                 </div>
             </div>
         </div>
 
-		<h2>Livraison</h2>
-        <div class="panel panel-default">
+		<div class="panel panel-default">
             <div class="panel-heading" role="tab" id="headingFive">
 
                 <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
                    href="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
                     <h4 class="panel-title">
-                        Quels sont les frais et délais de livraisons ? 
+                        Est-il possible de faire des retours ? 
                     </h4>
                 </a>
 
             </div>
             <div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
                 <div class="panel-body">
-                    <p>A voir avec JSPneus
+                    <p>Les retours ne sont pas possibles.
                     </p>
                 </div>
             </div>
         </div>
 
+		
+		<h2 style="color:white">Livraison</h2>
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="headingSix">
 
                 <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
                    href="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
                     <h4 class="panel-title">
-                        Restrictions de livraison
+                        Quels sont les frais et délais de livraisons ? 
                     </h4>
                 </a>
 
             </div>
             <div id="collapseSix" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSix">
                 <div class="panel-body">
-                    <p>A voir avec JSPneus
+                    <p>Les pneus vous seront livrés dans un délai de 48 à 72h. La livraison est gratuite en France métropolitaine dès l'achat de 2 pneus. Des frais d'expédition de 5€ seront à payer pour l'achat d'un seul pneu.
                     </p>
                 </div>
             </div>
         </div>
 
-		<h2>Votre compte</h2>
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="headingSeven">
 
                 <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
                    href="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
                     <h4 class="panel-title">
-                        Sécurité et confidentialité
+                        Restrictions de livraison
                     </h4>
                 </a>
 
             </div>
             <div id="collapseSeven" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSeven">
                 <div class="panel-body">
-                    <p>Les informations relatives au stockage de vos données personnelles se trouvent dans les conditions générales de vente et dans la section des mentions légales.
+                    <p>Les livraisons sont possibles en France métropolitaine uniquement.
                     </p>
                 </div>
             </div>
         </div>
 
+		<h2 style="color:white">Votre compte</h2>
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="headingEight">
 
                 <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
                    href="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
                     <h4 class="panel-title">
-                       Comment créer un compte ? 
+                        Sécurité et confidentialité
                     </h4>
                 </a>
 
             </div>
             <div id="collapseEight" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingEight">
                 <div class="panel-body">
-                    <p>Il vous suffit de cliquer sur la rubrique d'inscription dans la barre de navigation.Vous serez redirigé vers la page d'inscription. Il vous faudra donc remplir les champs relatifs à l'inscription. Les informations rentrées doivent être authentiques car elles seront utilisées lors de des commandes et autres opérations du site. 
+                    <p>Les informations relatives au stockage de vos données personnelles se trouvent dans les conditions générales de vente et dans la section des mentions légales.
                     </p>
                 </div>
             </div>
@@ -182,14 +183,14 @@ include_once("header.php");
                 <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
                    href="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
                     <h4 class="panel-title">
-                        Comment me déconnecter du site ? 
+                       Comment créer un compte ? 
                     </h4>
                 </a>
 
             </div>
             <div id="collapseNine" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingNine">
                 <div class="panel-body">
-                    <p>Dans la barre de navigation, si vous êtes connecté, l'option "Mon Compte" apparait, cliquez dessus pour obtenir les options de paramètrages de compte, de panier et de déconnexion. Chosissez "déconnexion" si vous souhaitez mettre fin à votre session.
+                    <p>Il vous suffit de cliquer sur la rubrique d'inscription dans la barre de navigation.Vous serez redirigé vers la page d'inscription. Il vous faudra donc remplir les champs relatifs à l'inscription. Les informations rentrées doivent être authentiques car elles seront utilisées lors de des commandes et autres opérations du site. 
                     </p>
                 </div>
             </div>
@@ -201,12 +202,31 @@ include_once("header.php");
                 <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
                    href="#collapseTen" aria-expanded="false" aria-controls="collapseTen">
                     <h4 class="panel-title">
-                        Comment changer mon nom, mon adresse, mon mot de passe ou mon numéro de téléphone ?  
+                        Comment me déconnecter du site ? 
                     </h4>
                 </a>
 
             </div>
             <div id="collapseTen" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTen">
+                <div class="panel-body">
+                    <p>Dans la barre de navigation, si vous êtes connecté, l'option "Mon Compte" apparait, cliquez dessus pour obtenir les options de paramètrages de compte, de panier et de déconnexion. Chosissez "déconnexion" si vous souhaitez mettre fin à votre session.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="headingEleven">
+
+                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
+                   href="#collapseEleven" aria-expanded="false" aria-controls="collapseEleven">
+                    <h4 class="panel-title">
+                        Comment changer mon nom, mon adresse, mon mot de passe ou mon numéro de téléphone ?  
+                    </h4>
+                </a>
+
+            </div>
+            <div id="collapseEleven" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingEleven">
                 <div class="panel-body">
                     <p>Dans la barre de navigation, si vous êtes connecté, l'option "Mon Compte" comprend l'option de paramètrage de compte. Cliquez sur cette option pour arriver sur la page de modification de compte. Pour cela modifiez les champs que vous souhaitez changer et validez grâce au bouton en bas de la page.
                     </p>
@@ -215,17 +235,17 @@ include_once("header.php");
         </div>
 
 		<div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="headingNine">
+            <div class="panel-heading" role="tab" id="headingTwelve">
 
                 <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
-                   href="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+                   href="#collapseTwelve" aria-expanded="false" aria-controls="collapseTwelve">
                     <h4 class="panel-title">
                         J'ai oublié mon mot de passe
                     </h4>
                 </a>
 
             </div>
-            <div id="collapseNine" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingNine">
+            <div id="collapseTwelve" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwelve">
                 <div class="panel-body">
                     <p>Si vous avez oublié votre mot de passe, l'option "Mot de passe oublié" dans l'onglet de connexion vous permettra de retrouver votre mot de passe en fournissant votre adresse e-mail d'inscription au site. Un e-mail avec un mot de passe temporaire vous sera envoyé et vous permettra donc de vous reconnecter. Nous vous conseillons de modifier votre mot de passe après être passé par cette option.
                     </p>
