d7e182788170623c0e8c4f9669417e14a33dd092
Martin Canivez <martin.canivez@ig2i.fr>
2016-03-31 22:41:33 +0200

    Ajout de l'AJAX d'obtention du ratio de prix

--- assets/php/ajax/config.php
+++ assets/php/ajax/config.php
@@ -1,7 +1,38 @@
 <?php
+
+include_once "../fonctions/AJAX.php";
+include_once "../fonctions/Config.php";
+
 /**
- * Created by PhpStorm.
- * User: Martin
- * Date: 31/03/2016
- * Time: 22:21
+ * Fichier utilisant la méthode "POST"
+ * Actions possible :
  */
+
+/**
+ * "getRatio"
+ * Renvoie le ratio actuel de prix, ou le prix associé à un temps donné
+ * Arguments :
+ * [    "ratioID" => ID du ratio *facultatif, renvoie le dernier ratio par défaut* ]
+ * Renvoi :
+ * [    "ratio" => Le ratio demandé ]
+ */
+
+if (!isset($_POST["action"]))
+{
+    ajaxError("Action non définie");
+}
+
+$action = $_POST["action"];
+
+switch ($action)
+{
+    case "getRatio" :
+
+        $ratioID = isset($_POST["ratioID"]) ? $_POST["ratioID"] : null;
+        ajaxSuccess(["ratio" => Config::getRatioPrix($ratioID)]);
+
+        break;
+
+    default:
+        ajaxError("Action inconnue");
+}
