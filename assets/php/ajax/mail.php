961ade94c4513614f57a854dcd7606328bb2793f
Andoslash <andoslash@hotmail.fr>
2016-04-01 11:43:59 +0200

    correction mail
    correction pour le mail avec un entete

--- assets/php/ajax/mail.php
+++ assets/php/ajax/mail.php
@@ -11,21 +11,33 @@ if (!isset($_POST["action"]))
     ajaxError("Action non définie");
 }
 $action = $_POST["action"];
-$headers = 'From:'. $_POST["from_email"] . "\r\n" .
-     'Reply-To:' . $_POST["from_email"] . "\r\n" .
-     'X-Mailer: PHP/' . phpversion();
+
+if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail))
+{
+	$passage_ligne = "\r\n";
+}
+else
+{
+	$passage_ligne = "\n";
+}
+
+
 
 $tab["message"]="Votre mail a bien été envoyé";
 switch ($action)
 {
 	case "mail_contact" :
+		
 		if ((!isset($_POST["from_email"])) || (!isset($_POST["from_name"])) || (!isset($_POST["subject"])) || (!isset($_POST["html"])))
 		{
 			ajaxError("Des informations sont manquantes", "MISSING_ARGUMENTS");
-			console.log("erreur de contact_mail");
 		}
 		else{
-			mail("martin.canivez@gmail.com", $_POST["subject"], $_POST["html"], $headers);
+		$header = "From: ". $_POST["from_name"] . $_POST["from_email"].$passage_ligne;
+		$header .= "Reply-to:"$_POST["from_name"] . $_POST["from_email"].$passage_ligne;
+		$header .= "MIME-Version: 1.0".$passage_ligne;
+		$header .= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+			mail("bonpneus@hotmail.fr", $_POST["subject"], $_POST["html"], $headers);
 			ajaxSuccess($tab);
 		}
 		break;
@@ -37,7 +49,7 @@ switch ($action)
 			
 		}
 		else {
-			ajaxSuccess("ayé ça marche");
+			ajaxSuccess($tab);
 		}
 		break;
 		
