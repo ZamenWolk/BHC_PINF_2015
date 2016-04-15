1c8e2dcddda1d62f58a6a08750d472572376ddeb
Evolus3r <samonil@hotmail.fr>
2016-04-15 15:31:37 +0200

    -Correction bug mail

--- www/assets/php/ajax/mail.php
+++ www/assets/php/ajax/mail.php
@@ -11,40 +11,114 @@ if (!isset($_POST["action"]))
     ajaxError("Action non définie");
 }
 $action = $_POST["action"];
-
-$mail_contact = "martin.canivez@gmail.com";
+if(isset($_POST["to_email"]))
+    $mail_contact = $_POST["to_email"];
+else
+    $mail_contact = "contact@js-pneus.fr";
 
 
 $tab["message"]="Votre mail a bien été envoyé";
 switch ($action)
 {
 	case "mail_contact" :
+        //fonctionnel
+        if(isset($_POST["from_email"]) && isset($_POST["from_name"]) && isset($_POST["subject"]) && isset($_POST["html"])) {
 
-		if (!preg_match('^[a-z0-9._-]+@(hotmail|live|msn)\.[a-z]{2,4}$', $mail_contact))
-		{
-			$passage_ligne = "\r\n";
-		}
-		else
-		{
-			$passage_ligne = "\n";
-		}
-		
-		if ((!isset($_POST["from_email"])) || (!isset($_POST["from_name"])) || (!isset($_POST["subject"])) || (!isset($_POST["html"])))
-		{
-			ajaxError("Des informations sont manquantes", "MISSING_ARGUMENTS");
-		}
-		else{
-		$header = "From: ". $_POST["from_name"] . ' <' . $_POST["from_email"]. '>' . $passage_ligne;
-		$header .= "Reply-to: ". $_POST["from_name"] . ' <' . $_POST["from_email"]. '>' .$passage_ligne;
-		$header .= "MIME-Version: 1.0".$passage_ligne;
-		$header .= "Content-Type: multipart/alternative;".$passage_ligne;
-			$res = mail($mail_contact, $_POST["subject"], $_POST["html"], $header);
 
-            if ($res)
-			    ajaxSuccess($tab);
+            if($_POST["from_email"] == "" || !strstr($_POST["from_email"], "@"))
+
+                ajaxError("mauvaise email", "WRONG_EMAIL");
+
+
+            if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail_contact)) // On filtre les serveurs qui rencontrent des bogues.
+
+            {
+
+                $passage_ligne = "\r\n";
+
+            } else {
+
+                $passage_ligne = "\n";
+
+            }
+
+//=====Déclaration des messages au format texte et au format HTML.
+
+            $message_txt = $_POST["html"];
+
+            $message_html = "<html><head></head><body>" . $_POST["html"] . "</body></html>";
+
+//==========
+
+
+//=====Création de la boundary
+
+            $boundary = "-----=" . md5(rand());
+
+//==========
+
+
+//=====Définition du sujet.
+
+            $sujet = $_POST["subject"];
+
+//=========
+
+
+//=====Création du header de l'e-mail.
+
+            $header = "From: \"" . $_POST["from_name"] . "\"<" . $_POST["from_email"] . ">" . $passage_ligne;
+
+            $header .= "MIME-Version: 1.0" . $passage_ligne;
+
+            $header .= "Content-Type: multipart/alternative;" . $passage_ligne . " boundary=\"$boundary\"" . $passage_ligne;
+
+//==========
+
+
+//=====Création du message.
+
+            $message = $passage_ligne . "--" . $boundary . $passage_ligne;
+
+            //=====Ajout du message au format texte.
+
+                    $message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
+
+                    $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+
+                    $message.= $passage_ligne.$message_txt.$passage_ligne;
+
+            //==========
+
+                    $message.= $passage_ligne."--".$boundary.$passage_ligne;
+
+//=====Ajout du message au format HTML
+
+           /* $message .= "Content-Type: text/html; charset=\"ISO-8859-1\"" . $passage_ligne;
+
+            $message .= "Content-Transfer-Encoding: 8bit" . $passage_ligne;
+
+            $message .= $passage_ligne . $message_html . $passage_ligne;*/
+
+//==========
+
+            $message .= $passage_ligne . "--" . $boundary . "--" . $passage_ligne;
+
+            $message .= $passage_ligne . "--" . $boundary . "--" . $passage_ligne;
+
+//==========
+
+
+//=====Envoi de l'e-mail.
+
+            if (mail($mail_contact, $sujet, $message, $header))
+                ajaxSuccess($tab);
             else
-                ajaxError("Le mail n'a pas été envoyé");
-		}
+                ajaxError("Non envoye", "NOT_SEND");
+        }
+        else
+            ajaxError("Champs manquant", "MISSING_ARGUMENTS");
+//==========
 		break;
 		
 	case "newsletter" : 
--- www/contact.php
+++ www/contact.php
@@ -2,7 +2,6 @@
 include_once('header.php');
 ?>
 
-
     <form class="form">
         <h1> Contact </h1>
         <a href="./faq">
@@ -35,20 +34,34 @@ include_once('header.php');
                 </div>
             </div>
         </div>
+
         <div class="form-group">
             <label for="message" class="sr-only">Message</label>
             <textarea id="message" class="form-control" placeholder="Composez votre message ici ..."></textarea>
         </div>
+
+
         <button id="BoutonEnvoi" type="button" class="btn btn-block btn-default btn-lg"><i class="fa fa-envelope-o"></i>
             Envoyer votre message
         </button>
 
+
+
+        <div style="display:none" id="con_alert_msg_failure" class="alert alert-danger" role="alert">
+            Le mail n'a pas été envoyé, veuillez remplir tous les champs.
+        </div>
+        <div style="display:none" id="con_alert_msg_succes" class="alert alert-success" role="alert">
+            Le mail a bien été envoyé. Nous vous répondrons dans les plus bref délais.
+        </div>
     </form>
+
 
     <script>
 
         $(document).ready(function () {
             var html;
+            var succes = $("#con_alert_msg_succes");
+            var failure = $("#con_alert_msg_failure");
 
             $("#message").change(function(){
                 html=this.value;
@@ -56,9 +69,9 @@ include_once('header.php');
 
 
             $("#BoutonEnvoi").click(function () {
-
-                console.log(html);
-                $.post('../assets/php/ajax/mail.php',
+                succes.hide('slow');
+                failure.hide('slow');
+                $.post('assets/php/ajax/mail.php',
                     {
                         action: "mail_contact",
                         from_email: $('#mail').val(),
@@ -67,7 +80,20 @@ include_once('header.php');
                         html: html
                     },
                     function (data) {
-                        console.log(data);
+
+                        data = JSON.parse(data);
+                        //console.log(data);
+
+                        if(data.etat == "reussite")
+                        {
+                            succes.show('slow');
+                            failure.hide('slow');
+                        }
+                        else
+                        {
+                            succes.hide('slow');
+                            failure.show('slow');
+                        }
                      });
 
             });
